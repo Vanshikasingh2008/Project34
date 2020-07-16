2e27f7bbad2171044eb8e6e3091a9bd47d8845e2
Vanshikasingh2008 <64592848+Vanshikasingh2008@users.noreply.github.com>
2020-07-16 22:09:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+//Create variables here
+var dog, happyDog, database, foodLS, foodStock;
+
+function preload()
+{
+  //load images here
+  happyDog = loadImage("happydog.png");
+  dog1 = loadImage("Dog1.png");
+  
+}
+
+function setup() {
+  createCanvas(500,500);
+  
+  dog = createSprite(250,250,10,10);
+  dog.addImage("normal",dog1);
+  dog.scale = 0.3;
+
+  database = firebase.database();
+  
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
+
+  /*foodEaten = database.ref('EatenFood');
+  foodEaten.on("value", readStock);*/
+}
+
+
+function draw() { 
+  background(46, 139, 87); 
+
+  if(keyWentDown(UP_ARROW)){
+    dog.addImage(happyDog);
+    writeStock(foodLS);
+    //eatenFood(foodES);
+  }
+
+  drawSprites();
+  //add styles here
+  textSize(20);
+  fill("white");
+  stroke("black");
+  text("Note : Press Up_Arrow to feed the Dog",75,30);
+  text("Food Left to eat = "+ foodLS,100,430);
+  //text("Food already eaten = " + foodES,100,460);
+
+}
+
+function readStock(data){
+  foodLS = data.val();
+  //foodES = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x = 0;
+  }else{
+    x = x-1;
+  }
+  database.ref('/').update({
+    Food : x
+  })
+}
+
+/*function eatenFood(){
+  if(x>=20){
+    x = 20;
+  }else{
+    x = x+1;
+  }
+  database.ref('/').update({
+    Food : x
+  })
+}*/
